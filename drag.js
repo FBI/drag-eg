0c8a0a110585db61849cba1db2ed7afacc121acd
FBI <18311433156@163.com>
2019-04-18 14:14:32 +0800

    latest commit

--- drag.js
+++ drag.js
@@ -1,5 +1,6 @@
 // git checkout commit-id <file> 恢复某个文件
 // git reset HEAD <file> 撤消暂存
+// git checkout commit-id <file> 恢复工作区被删除的文件
 // git diff 查看工作区与暂存区的区别
 // git diff --cached 查看暂存区与本地版本库的区别
 // git diff master 查看工作区与本地版本库的区别
@@ -7,11 +8,12 @@
 // git rm <file> 删除暂存区文件(前提是先把文件提交到暂存区，然后再删除工作区文件，再使用该命令)
 // git rm -rf <file> 同时删除工作区和暂存区文件
 // git rm --cached 删除暂存区文件，但是保留工作区文件，不让该文件被版本控制
+// git commit -m "..." --amend 撤消上次提交并重新提交  撤消的是提交到本地版本库的操作回到暂存区然后重新提交
 function drag(id) {
     var obj = document.getElementById(id)
     var disX = 0
     var disY = 0
     obj.onmousedown = function() {
-        console.log(222)
+        console.log(229992)
     }
 }
